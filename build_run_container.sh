d6bd2e37d636396552c21fa153735fbd4ad1c6d5
Ziro41 <82183046+Ziro41@users.noreply.github.com>
2025-06-22 23:08:11 -0300

    Update build_run_container.sh

--- build_run_container.sh
+++ build_run_container.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
 docker build --tag 'vulnerable_cronjob' .
-docker run --cap-add NET_ADMIN --device /dev/net/tun  -it --name vulnerable_cron vulnerable_cron zsh
+docker run --cap-add NET_ADMIN --device /dev/net/tun  -it --name vulnerable_cronjob vulnerable_cronjob zsh
